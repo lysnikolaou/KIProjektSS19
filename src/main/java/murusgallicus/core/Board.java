4d90baf274948cb259a9db712458958e74cfd6b7
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-04-18 01:51:01 +0200

    Intermediate commit, that's it for today

--- src/main/java/murusgallicus/core/Board.java
+++ src/main/java/murusgallicus/core/Board.java
@@ -1,5 +1,8 @@
 package murusgallicus.core;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 class Board {
 
   /**
@@ -14,18 +17,25 @@ class Board {
     GaulCatapult
   }
 
+  /**
+   * The board.
+   */
   Piece[][] board;
 
   /**
    * murusgallicus.base.Board constructor that converts a FEN string to its board representation.
-   * @param fen The FEN representation of the board.
+   * @param fen The FEN representation of the board
    */
   Board(String fen) {
     board = new Piece[7][8];
     setBoard(fen);
   }
 
-  void setBoard(String fen) {
+  /**
+   * Set board array from FEN string.
+   * @param fen THE FEN string that should be stored into the board
+   */
+  private void setBoard(String fen) {
     int row = 0;
     int column = 0;
     int fenLength = fen.length();
@@ -45,6 +55,75 @@ class Board {
     }
   }
 
+  /**
+   * The actual move generator.
+   * @param player The player, whose turn it is to play('r' for Romans, 'g' for Gauls)
+   * @return A list containing all the available moves
+   */
+  ArrayList<Move> generateMoves(char player) {
+    if (player == 'r') {
+      return generateRomanMoves();
+    } else if (player == 'g') {
+      return generateGaulMoves();
+    } else {
+      throw new IllegalArgumentException("Player must be either 'r' for Romans or 'g' for Gauls.");
+    }
+  }
+
+  /**
+   * The move generator for the Roman player.
+   * @return A list containing all the available moves for the roman player
+   */
+  private ArrayList<Move> generateRomanMoves() {
+    ArrayList<Move> moves = new ArrayList<>();
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        if (board[i][j] == Piece.RomanTower)
+          moves.addAll(generateRomanTowerMoves());
+        else if (board[i][j] == Piece.RomanCatapult)
+          moves.addAll(generateRomanCatapultMoves());
+      }
+    }
+    return moves;
+  }
+
+  /**
+   * The move generator for the Gaul player.
+   * @return A list containing all the available moves for the gaul player
+   */
+  private ArrayList<Move> generateGaulMoves() {
+    ArrayList<Move> moves = new ArrayList<>();
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        if (board[i][j] == Piece.GaulTower)
+          moves.addAll(generateGaulTowerMoves());
+        else if (board[i][j] == Piece.GaulCatapult)
+          moves.addAll(generateGaulCatapultMoves());
+      }
+    }
+    return moves;
+  }
+
+  private ArrayList<Move> generateGaulTowerMoves() {
+    return null;
+  }
+
+  private ArrayList<Move> generateGaulCatapultMoves() {
+    return null;
+  }
+
+  private ArrayList<Move> generateRomanTowerMoves() {
+    return null;
+  }
+
+  private ArrayList<Move> generateRomanCatapultMoves() {
+    return null;
+  }
+
+  /**
+   * Convert the board to a FEN string.
+   * @return The FEN representation of the board
+   */
   @Override
   public String toString() {
     StringBuilder fenBuilder = new StringBuilder();
@@ -63,6 +142,15 @@ class Board {
     return fenBuilder.toString();
   }
 
+  /**
+   * This method gets called when en empty square is reached on the board. What this does, is counts
+   * the next empty squares and returns that number(This needs to be done, so that the number of
+   * empty squares gets appended to the FEN String).
+   * @param row The row index of the first empty square
+   * @param column The column index of the first empty square
+   * @param fenBuilder The StringBuilder, which the number has to be appended to
+   * @return The number of adjacent empty squares
+   */
   private int countEmptySquaresAndUpdateFen(int row, int column, StringBuilder fenBuilder) {
     int emptySquares = 0;
     while (column < 8 && board[row][column++] == null) {
@@ -72,6 +160,12 @@ class Board {
     return emptySquares;
   }
 
+  /**
+   * Get the appropriate FEN character according to the piece enum.
+   * @param piece The piece to convert
+   * @return The FEN character that represent the piece type
+   * @throws IllegalArgumentException When the piece is of an unknown type
+   */
   private char getCharFromPiece(Piece piece) throws IllegalArgumentException {
     switch (piece) {
       case RomanWall:
@@ -91,6 +185,12 @@ class Board {
     }
   }
 
+  /**
+   * Set the appropriate piece enum type according to the FEN character.
+   * @param fen_char The FEN character to be converted into a piece
+   * @param row The row index of the piece
+   * @param column The column index of the piece
+   */
   private void setPieceFromChar(char fen_char, int row, int column) {
     switch (fen_char) {
       case 'W':
---
+++ src/main/java/murusgallicus/core/Move.java
@@ -0,0 +1,14 @@
+package murusgallicus.core;
+
+class Move {
+
+  /**
+   * The source square of the moving piece
+   */
+  String sourceSquare;
+
+  /**
+   * The destination square of the moving piece
+   */
+  String destinationSquare;
+}
