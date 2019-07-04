d6fbf3073377e610fcd23562ffacca6440c71ceb
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-07-04 21:28:35 +0200

    Debugging python models (#19)

--- scripts/model.py
+++ scripts/model.py
@@ -1,32 +1,97 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import sys
+import enum
 from itertools import product
-from sys import argv, exit
 
+import joblib
 import numpy as np
+import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.externals import joblib
 from dbn.tensorflow import UnsupervisedDBN
 
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 DBN_NETWORK_FILENAME = 'dbn_model.pkl'
 MLP_NETWORK_FILENAME = 'mlp_model.pkl'
 DATA_FILENAME = '/home/ubuntu/repos/KIProjektSS19/scripts/data/data.csv'
 
 
-def set_piece_at_square(bitboards, squarecounter, c):
+class Square(enum.IntEnum):
+    a7 = 6
+    b7 = 13
+    c7 = 20
+    d7 = 27
+    e7 = 34
+    f7 = 41
+    g7 = 48
+    h7 = 55
+    a6 = 5
+    b6 = 12
+    c6 = 19
+    d6 = 26
+    e6 = 33
+    f6 = 40
+    g6 = 47
+    h6 = 54
+    a5 = 4
+    b5 = 11
+    c5 = 18
+    d5 = 25
+    e5 = 32
+    f5 = 39
+    g5 = 46
+    h5 = 53
+    a4 = 3
+    b4 = 10
+    c4 = 17
+    d4 = 24
+    e4 = 31
+    f4 = 38
+    g4 = 45
+    h4 = 52
+    a3 = 2
+    b3 = 9
+    c3 = 16
+    d3 = 23
+    e3 = 30
+    f3 = 37
+    g3 = 44
+    h3 = 51
+    a2 = 1
+    b2 = 8
+    c2 = 15
+    d2 = 22
+    e2 = 29
+    f2 = 36
+    g2 = 43
+    h2 = 50
+    a1 = 0
+    b1 = 7
+    c1 = 14
+    d1 = 21
+    e1 = 28
+    f1 = 35
+    g1 = 42
+    h1 = 49
+
+
+def set_piece_at_square(bitboards, square, c):
     if c == 'w':
-        bitboards[squarecounter] = 1
+        bitboards[square.value] = 1
     elif c == 't':
-        bitboards[56+squarecounter] = 1
+        bitboards[56+square.value] = 1
     elif c == 'c':
-        bitboards[2*56+squarecounter] = 1
+        bitboards[2*56+square.value] = 1
     elif c == 'W':
-        bitboards[3*56+squarecounter] = 1
+        bitboards[3*56+square.value] = 1
     elif c == 'T':
-        bitboards[4*56+squarecounter] = 1
+        bitboards[4*56+square.value] = 1
     elif c == 'C':
-        bitboards[5*56+squarecounter] = 1
+        bitboards[5*56+square.value] = 1
 
 
 def encode_position(fen: str):
@@ -35,12 +100,13 @@ def encode_position(fen: str):
     player = board_and_player[1]
 
     bitboards = np.zeros(337)
+    squares_fen_order = list(Square)
     squarecounter = 0
     for c in board:
         if c.isdigit():
             squarecounter += int(c)
         elif c != '/':
-            set_piece_at_square(bitboards, squarecounter, c)
+            set_piece_at_square(bitboards, squares_fen_order[squarecounter], c)
             squarecounter += 1
     bitboards[336] = 0 if player == 'r' else 1
     return bitboards
@@ -82,9 +148,8 @@ def predict_mlp(first, second):
     first_position = dbn_network.transform(First)
     second_position = dbn_network.transform(Second)
     X = np.concatenate((first_position, second_position))
-    X = dbn_network.transform(X.reshape(1, -1))
-    result = mlp_network.predict(X)
-    return list(result)
+    result = mlp_network.predict(X.reshape(1, -1))
+    return result.flatten().tolist()
 
 
 def extract_mlp_data(data):
@@ -130,12 +195,12 @@ def test_mlp(X, Y):
 
 
 def main():
-    if argv[1] == 'predict':
-        first = list(map(int, argv[2][1:-1].split(', ')))
-        second = list(map(int, argv[3][1:-1].split(', ')))
+    if len(sys.argv) >= 2 and sys.argv[1] == 'predict':
+        first = list(map(int, sys.argv[2][1:-1].split(',')))
+        second = list(map(int, sys.argv[3][1:-1].split(',')))
         result = predict_mlp(first, second)
         print(str(result))
-        exit(0)
+        sys.exit(0)
 
     dbn_data, mlp_data, mlp_labels = gather_data()
     print("Splitting data in Training and Test Data...")
@@ -146,13 +211,13 @@ def main():
     )
     print("Splitting Done!")
 
-    # print("Training Deep Belief Network...")
-    # train_dbn(dbn_data)
-    # print("Training DBN Done!")
+    print("Training Deep Belief Network...")
+    train_dbn(dbn_data)
+    print("Training DBN Done!")
 
-    # print("Training Multi-Layer Perceptron...")
-    # train_mlp(X_train, Y_train)
-    # print("Training MLP Done!")
+    print("Training Multi-Layer Perceptron...")
+    train_mlp(X_train, Y_train)
+    print("Training MLP Done!")
 
     print("Testing MLP...")
     print(f"Score = {test_mlp(X_test, Y_test)}")
